33e5cdffa800b096bb8c0efb5df43eba3f4e92ac
JayDevL <codejd.6@gmail.com>
2024-11-17 19:54:32 -0600

    update3

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from 'express';
 
 const app = express();
 
-const port = 80;
+const port = 8080;
 
 app.get('/',(req,res) => {
     res.send('<h1>Hello world from express and running on AWS Services in a instance WaterWaveProject</h1>')
